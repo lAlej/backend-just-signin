552058fcb3571cc2a629e3fd54044368725834cd
Luis Peña <luis.pena@getback.tech>
2024-01-27 22:56:06 -0400

    fix update jwt

--- middlewares/updateJWT.js
+++ middlewares/updateJWT.js
@@ -4,11 +4,11 @@ const mysql = require("mysql2/promise");
 
 const updateJWT = (jwt, username) => {
   mysql.createConnection({
-    host: "sql3.freemysqlhosting.net",
-    user: "sql3678855",
-    port: 3306,
-    password: "paV24S5Kl2",
-    database: "sql3678855",
+    host: "monorail.proxy.rlwy.net",
+    user: "root",
+    port: 21751,
+    password: "EcBd5E2FehaBcA3d1bf-2eC51EAHecGf",
+    database: "just_sign_in",
   })
   .then((conn) => 
     conn.query(`UPDATE users_login SET jwt = ? WHERE username = ?`, [jwt, username])
